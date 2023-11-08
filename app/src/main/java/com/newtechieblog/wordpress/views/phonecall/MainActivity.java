f33a8f13e9c5cf3ef2f218f9bcad6496de820eb4
Kelvin Ricafort <kelvs.r@outlook.com>
2023-11-08 15:15:53 +0800

    Finish

--- app/src/main/java/com/newtechieblog/wordpress/views/phonecall/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/phonecall/MainActivity.java
@@ -1,14 +1,63 @@
 package com.newtechieblog.wordpress.views.phonecall;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCall;
+    EditText editTextNumber;
+
+    String userNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCall = findViewById(R.id.btnCall);
+        editTextNumber = findViewById(R.id.editTextNumber);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userNumber = editTextNumber.getText().toString();
+
+                phoneCall(userNumber);
+            }
+        });
+    }
+
+    public void phoneCall(String userNumber) {
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CALL_PHONE}, 100);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:" + userNumber));
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 100 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:" + userNumber));
+            startActivity(intent);
+        }
     }
 }
